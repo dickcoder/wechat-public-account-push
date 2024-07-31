2cdf060436d38a5efd4150d6feb81ef2e52b9f17
dickcoder <82507880+dickcoder@users.noreply.github.com>
2024-07-31 15:00:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'HWO-Ap2PekuI2RKk1U-YPYQ89a4MnD6LDFKweJPj-cw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o1mgB68NPgw2iSGWHp7mhclP29N0',
     }
   ],
 
